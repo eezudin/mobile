9579b15f22d0ae1b4415b39e8a430e406d904db1
Evgeniy Zudin <evgeniy.zudin@mera.com>
2023-05-05 13:46:40 +0200

    create fixture: from unittest to pytest

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from application import Application
-import unittest
+import pytest
 
-class test_example(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
 
-
-    def test_example(self):
-        self.app.open_page('https://www.google.com/')
-        self.app.clear_search_form()
-        self.app.fill_search_form('figujhblkfjgbnlfkj')
-
-    def tearDown(self):
-        self.app.destroy()
-
-
-
+def test_example(app):
+    app.open_page('https://www.google.com/')
+    app.clear_search_form()
+    app.fill_search_form('figujhblkfjgbnlfkj')
