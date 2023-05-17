6fcda48bc9466f688eb8501f5584076329be2bcb
Evgeniy Zudin <evgeniy.zudin@mera.com>
2023-05-17 16:47:27 +0200

    add start appium server

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,18 @@
 import pytest
 from helpers.application import Application
+from appium.webdriver.appium_service import AppiumService
 
 
 @pytest.fixture
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+@pytest.fixture(scope='session', autouse=True)
+def appium_server(request):
+    fixture = AppiumService()
+    fixture.start()
+    request.addfinalizer(fixture.stop)
     return fixture
